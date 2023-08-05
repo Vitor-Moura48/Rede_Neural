46b1f4e485a8c88e514482f9b880dae30b2ede2c
Vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-08-05 18:08:06 -0300

    Tirando variaveis globais (para o novo arquivo)

--- Config.py
+++ Config.py
@@ -22,30 +22,8 @@ velocidade_ia = 6  # 9 max no caso (10 inimogo, 10 player) 10 + 9 = 19  -- 20 (1
 tela = pygame.display.set_mode((largura, altura))
 tela.fill((000, 000, 000))
 
-# ajuda a selecionar o melhor individuo de cada geração
-melhor_tempo = 0
 
-melhor_peso_primeira_camada_oculta = []
-melhor_peso_camada_de_saida = []
+fonte = pygame.font.Font(None, 32)
+mensagem_fps_para_tela = fonte.render('fps 0', True, (255, 000, 000))
 
-contador_geracoes = 0
-
-geracao_avo = []
-geracao_anterior = []
-geracao_atual = []
-
-juncao_de_geracoes = []
-valores_proporcionais = []
-
-ja_sorteados = []
-
-# variavel para ajudar a manter sempre o melhor player
-primeiro_individuo = True
-
-# variavel para ajudar a direcionar os primeiros projeteis exatamente na direção do surgimento do player, para eliminar os piores
-primeiro_inimigo = 0
-
-# lista para juntar os objetos das classes
-grupo_inimigos = []
-grupo_players = []
 
