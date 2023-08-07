829f77f1f6d3efb8ba86c73649ca6de1ee066d86
Vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-08-07 16:57:38 -0300

    Recriação dos individuos em qualquer configuração

--- Rede_Neural/Criação_de_Rede.py
+++ Rede_Neural/Criação_de_Rede.py
@@ -1,74 +1,48 @@
-from Jogo.Config import *
-import Jogo.Variaveis_globais as Variaveis_globais
+from Config import *
+import Variaveis_globais as Variaveis_globais
 
 
 class CriarRedeNeural:
     def __init__(self):
 
-       # se for a primeira geração ele cria os pesos e randomiza-os
+        self.camadas = []
+
+        # lógica para criação mais automatizada da rede neural, variavel de definição rem Config 
+        
+        for camada in range(len(configuracao_de_camadas) - 1):
+            self.camadas.append([])
+
+            for neuronios in range(configuracao_de_camadas[camada + 1]):
+
+                    pesos = numpy.array([0] * configuracao_de_camadas[camada], dtype=float)
+                    self.camadas[-1].append(pesos) 
+
+
+       # se for a primeira geração ele randomiza todos os pesos
         if Variaveis_globais.contador_geracoes == 0:
-            self.resultado = self.criar_geracao()
+            self.criar_geracao()
             
         # caso não for a primeira geração, ele faz uma nova a partir da(s) anterior(es)
         else: 
-            self.resultado = self.reproduzir_geracao()
+            self.reproduzir_geracao()
         
 
     def criar_geracao(self):
-
-        self.grupo_neuronios_primeira_camada_oculta = []  # pesos da camada de entrada para a primeira camada oculta
-
-        pesos_para_primeiro_neuronio_da_primeira_camada_oculta = numpy.array([0] * 19, dtype=float)
-        self.grupo_neuronios_primeira_camada_oculta.append(pesos_para_primeiro_neuronio_da_primeira_camada_oculta)
-
-        pesos_para_segundo_neuronio_da_primeira_camada_oculta = numpy.array([0] * 19, dtype=float)
-        self.grupo_neuronios_primeira_camada_oculta.append(pesos_para_segundo_neuronio_da_primeira_camada_oculta)
-
-        pesos_para_terceiro_neuronio_da_primeira_camada_oculta = numpy.array([0] * 19, dtype=float)
-        self.grupo_neuronios_primeira_camada_oculta.append(pesos_para_terceiro_neuronio_da_primeira_camada_oculta)
-
-        pesos_para_quarto_neuronio_da_primeira_camada_oculta = numpy.array([0] * 19, dtype=float)
-        self.grupo_neuronios_primeira_camada_oculta.append(pesos_para_quarto_neuronio_da_primeira_camada_oculta)
-
-        pesos_para_quinto_neuronio_da_primeira_camada_oculta = numpy.array([0] * 19, dtype=float)
-        self.grupo_neuronios_primeira_camada_oculta.append(pesos_para_quinto_neuronio_da_primeira_camada_oculta)
-
-        self.grupo_neuronios_camada_de_saida = []  # pesos da primeira camada oculta para a camada de saida
-
-        pesos_para_primeiro_neuronio_da_camada_de_saida = numpy.array([0] * 5, dtype=float)
-        self.grupo_neuronios_camada_de_saida.append(pesos_para_primeiro_neuronio_da_camada_de_saida)
-
-        pesos_para_segundo_neuronio_da_camada_de_saida = numpy.array([0] * 5, dtype=float)
-        self.grupo_neuronios_camada_de_saida.append(pesos_para_segundo_neuronio_da_camada_de_saida)
-
-        pesos_para_terceiro_neuronio_da_camada_de_saida = numpy.array([0] * 5, dtype=float)
-        self.grupo_neuronios_camada_de_saida.append(pesos_para_terceiro_neuronio_da_camada_de_saida)
-
-        pesos_para_quarto_neuronio_da_camada_de_saida = numpy.array([0] * 5, dtype=float)
-        self.grupo_neuronios_camada_de_saida.append(pesos_para_quarto_neuronio_da_camada_de_saida)
-
+                
         # randomizando cada peso
-        for neuronio in range(len(self.grupo_neuronios_primeira_camada_oculta)):
-            for peso in range(19):
-                self.grupo_neuronios_primeira_camada_oculta[neuronio][peso] = round(uniform(-1, 1), 8)
-            self.grupo_neuronios_primeira_camada_oculta[neuronio][-1] = bias
+        for camada in range(len(self.camadas)):
+            for neuronio in range(len(self.camadas[camada])):
+                for peso in range(len(self.camadas[camada][neuronio])):
+                    self.camadas[camada][neuronio][peso] = round(uniform(-1, 1), 8)
+                    self.camadas[camada][neuronio][-1] = bias
 
-        for neuronio in range(len(self.grupo_neuronios_camada_de_saida)):
-            for peso in range(5):
-                self.grupo_neuronios_camada_de_saida[neuronio][peso] = round(uniform(-1, 1), 8)
-            self.grupo_neuronios_camada_de_saida[neuronio][-1] = bias
-        
-        return (self.grupo_neuronios_primeira_camada_oculta, self.grupo_neuronios_camada_de_saida)
-    
 
-    
     def reproduzir_geracao(self):
 
         # o melhor individuo sempre será passa do para a próxima geração
         if Variaveis_globais.primeiro_individuo:
-            self.grupo_neuronios_primeira_camada_oculta = copy.deepcopy(Variaveis_globais.melhor_peso_primeira_camada_oculta)
-            self.grupo_neuronios_camada_de_saida = copy.deepcopy(Variaveis_globais.melhor_peso_camada_de_saida)
-
+            self.camadas = Variaveis_globais.melhor_individuo
+            
             Variaveis_globais.primeiro_individuo = False
 
         # faz um sorteio dos individuos com preferencia dos melhores
@@ -99,59 +73,56 @@ class CriarRedeNeural:
                         meio = (primeiro + ultimo) // 2
 
                 return meio
-
+           
             # sorteia dois individuos
             roleta_1 = roleta()
             roleta_2 = roleta()
 
-            novo_individuo = [[], []]
 
+            # inicia o novo individuo com a mesma estrutura inicial 
+            novo_individuo = []
+
+         
             # junta caracteristicas dos dois individuos para formar o novo individuo
-            camada = randint(1, 2)
-
-            if camada == 1:
-
-                neuronio = randint(0, 18)
+            camada_insercao_escolhida = randint(0, len(self.camadas) - 1) 
+            neuronio_insercao_escolhida = randint(0, len(self.camadas[camada_insercao_escolhida]) - 1)
+            peso_insercao_escolhida = randint(0, len(self.camadas[camada_insercao_escolhida][neuronio_insercao_escolhida]) - 1)
 
 
-                novo_individuo[0] = Variaveis_globais.juncao_de_geracoes[roleta_1][1][:neuronio + 1]
-                if neuronio != 18:
-                    novo_individuo[0] += Variaveis_globais.juncao_de_geracoes[roleta_2][1][neuronio + 1:]
 
+            # une os dois individuos em 1
+            for camada in range(len(self.camadas)):
+                for neuronio in range(len(self.camadas[camada])):
 
-                novo_individuo[1] = Variaveis_globais.juncao_de_geracoes[roleta_2][2]
+                    if camada < camada_insercao_escolhida:
+                        self.camadas[camada][neuronio] = Variaveis_globais.juncao_de_geracoes[roleta_2][camada][neuronio]
+                    
+                    elif camada == camada_insercao_escolhida and neuronio < neuronio_insercao_escolhida: 
+                        self.camadas[camada][neuronio] = Variaveis_globais.juncao_de_geracoes[roleta_1][camada][neuronio]
+                
 
-            elif camada == 2:
-                neuronio = randint(0, 4)
+                    elif camada == camada_insercao_escolhida and neuronio >= neuronio_insercao_escolhida:
+                        self.camadas[camada][neuronio] = Variaveis_globais.juncao_de_geracoes[roleta_2][camada][neuronio]
+                    
+                    elif camada > camada_insercao_escolhida:
+                        self.camadas[camada][neuronio] = Variaveis_globais.juncao_de_geracoes[roleta_2][camada][neuronio]
+                    
+                    else:
+                        print('houve alguma exeção!!!!!!')
+            
 
-                novo_individuo[0] = Variaveis_globais.juncao_de_geracoes[roleta_1][1]
-
-                novo_individuo[1] = Variaveis_globais.juncao_de_geracoes[roleta_1][2][:neuronio + 1]
-                if neuronio != 4:
-                    novo_individuo[1] += Variaveis_globais.juncao_de_geracoes[roleta_2][2][neuronio + 1:]
-
-    
-            # adiciona cada conjunto de pesos nas variaveis
-            self.grupo_neuronios_primeira_camada_oculta = novo_individuo[0]
-            self.grupo_neuronios_camada_de_saida = novo_individuo[1]
-           
 
     def randomizar_resultados(self):
 
-        # percorre cada neuronio e cada peso do neuronio e randomiza-os
-        for neuronio in range(len(self.grupo_neuronios_primeira_camada_oculta)):
-
-            for peso in range(len(self.grupo_neuronios_primeira_camada_oculta[neuronio]) - 1):
-                if randint(1, 10) == 1:
-                    self.grupo_neuronios_primeira_camada_oculta[neuronio][peso] = round(uniform(-1, 1), 8)
-
-        for neuronio in range(len(self.grupo_neuronios_camada_de_saida)):
-
-            for peso in range(len(self.grupo_neuronios_camada_de_saida[neuronio]) - 1):
-                if randint(1, 10) == 1:
-                    self.grupo_neuronios_camada_de_saida[neuronio][peso] = round(uniform(-1, 1), 8)
+        # randomizando cada peso
+        for camada in range(len(self.camadas)):
+            for neuronio in range(len(self.camadas[camada])):
+                for peso in range(len(self.camadas[camada][neuronio]) - 1):
+                    if randint(1, 10) == 1:
+                        self.camadas[camada][neuronio][peso] = round(uniform(-1, 1))
+                    self.camadas[camada][neuronio][peso] = round(uniform(-1, 1), 8)
         
-        return (self.grupo_neuronios_primeira_camada_oculta, self.grupo_neuronios_camada_de_saida)
+        return (self.camadas)
 
     
                 
