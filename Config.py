5c1b597aaccabd61de5d0498e40039d381fadd82
Vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-08-12 14:05:18 -0300

    configuraçãoes para testar o desempenho da rede

--- Config.py
+++ Config.py
@@ -26,21 +26,22 @@ tela.fill((000, 000, 000))
 fonte = pygame.font.Font(None, 32)
 mensagem_fps_para_tela = fonte.render('fps 0', True, (255, 000, 000))
 
-arquivo = 0
+arquivo = 1
 
+# teste padrão
 if arquivo == 0:
     # define o valor do vies da rede neural
     bias = 1
     projeteis_para_entrada = 8
 
-    quantidade_entradas = (projeteis_para_entrada * 3) + 2
+    quantidade_entradas = (projeteis_para_entrada * 5) + 4
 
-    configuracao_de_camadas = (quantidade_entradas, 5, 4)
+    configuracao_de_camadas = (quantidade_entradas, 9, 4)
 
-    numero_inimigos = 20
+    numero_inimigos = 15
     numero_players = 1500
 
-    numero_de_elitismo = numero_players * 0.1
+    numero_de_elitismo = numero_players * 0.2
 
     taxa_de_mutacao_base = 0.1
 
@@ -49,41 +50,42 @@ if arquivo == 0:
 
 elif arquivo == 1:
     # define o valor do vies da rede neural
-    bias = 1
+    bias = 0
 
     projeteis_para_entrada = 4
 
-    quantidade_entradas = (projeteis_para_entrada * 3) + 2
+    quantidade_entradas = (projeteis_para_entrada * 5) + 4
 
-    configuracao_de_camadas = (quantidade_entradas, 5, 4)
+    configuracao_de_camadas = (quantidade_entradas, quantidade_entradas * 2, 4)
 
-    numero_inimigos = 20
+    numero_inimigos = 15
     numero_players = 1500
 
-    numero_de_elitismo = numero_players * 0.1
+    numero_de_elitismo = numero_players * 0.4
 
-    taxa_de_mutacao_base = 1
+    taxa_de_mutacao_base = 0.3
 
-    recompensa_objetivo = 3000
+    recompensa_objetivo = 20000
 
+# teste com entradas minimas, elitismo médio, objetivo: 5000 loops
 elif arquivo == 2:
-    # define o valor do vies da rede neural
+
     bias = 1
 
     projeteis_para_entrada = 1
 
-    quantidade_entradas = (projeteis_para_entrada * 3) + 2
+    quantidade_entradas = (projeteis_para_entrada * 5) + 4
 
-    configuracao_de_camadas = (quantidade_entradas, 10, 4)
+    configuracao_de_camadas = (quantidade_entradas, 18, 4)
 
-    numero_inimigos = 20
+    numero_inimigos = 15
     numero_players = 1500
 
-    numero_de_elitismo = numero_players * 0.1
+    numero_de_elitismo = numero_players * 0.5
 
-    taxa_de_mutacao_base = 1
+    taxa_de_mutacao_base = 0.1
 
-    recompensa_objetivo = 4000
+    recompensa_objetivo = 50000
 
 
 
