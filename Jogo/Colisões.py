6f8ee39d4d2fe44cf32991b66d4106c54fe4ce76
Vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-08-13 21:15:02 -0300

    condições para metodo de média de recompensa

--- Jogo/Colisões.py
+++ Jogo/Colisões.py
@@ -24,20 +24,19 @@ class Colisoes:
                     # obter o tempo de vida do individuo
                     tempo_de_vida = player.funcao_de_perda()
 
-                    # enviar os pesos do individuo para a lista da geração
-                    pesos_individuo = []
-                    pesos_individuo.append([tempo_de_vida])
-                    for camada in player.camadas:
-                        pesos_individuo.append(camada)
+                    if Variaveis_globais.partida_atual_da_geracao == 0:
+                      
+                        # enviar os pesos do individuo para a lista da geração
+                        pesos_individuo = []
+                        pesos_individuo.append([tempo_de_vida])
+                        for camada in player.camadas:
+                            pesos_individuo.append(camada)
 
-                   # coloca os pesos do individuo no indice escolhido no inicio da geração
-                    Variaveis_globais.geracao_atual[player.indice] = pesos_individuo
-
-                    # se o tempo de vida dele for maior que o do melhor individuo, ele se torna o melhor
-                    if tempo_de_vida > Variaveis_globais.melhor_tempo:
-                        Variaveis_globais.melhor_tempo = tempo_de_vida
-                        Variaveis_globais.melhor_individuo = player.camadas
-                        
+                    # coloca os pesos do individuo no indice escolhido no inicio da geração
+                        Variaveis_globais.geracao_atual[player.indice] = pesos_individuo
+                    
+                    else:
+                        Variaveis_globais.geracao_atual[player.indice][0][0] += tempo_de_vida
 
                     # avisa que o player já colidiu
                     self.colidiu = True
@@ -55,22 +54,23 @@ class Colisoes:
                 # se saiu, faz a mesma coisa da colisão com um inimigo
                 if player in Variaveis_globais.grupo_players:
                     Variaveis_globais.grupo_players.remove(player)
-
-                    # pune os individuos que colidiram com a tela
-                    tempo_de_vida = player.funcao_de_perda() - 1000
-
-                    pesos_individuo = []
-                    pesos_individuo.append([tempo_de_vida])
-                    for camada in player.camadas:
-                        pesos_individuo.append(camada)
                     
+                    # pune os individuos que colidiram com a tela
+                    tempo_de_vida = player.funcao_de_perda() * 0.8
 
-                    Variaveis_globais.geracao_atual[player.indice] = pesos_individuo
+                    if Variaveis_globais.partida_atual_da_geracao == 0:
+                       
+                        pesos_individuo = []
+                        pesos_individuo.append([tempo_de_vida])
+                        for camada in player.camadas:
+                            pesos_individuo.append(camada)
+                        
 
-                    # so vai para "pódio" de melhor individuo se for pelo menos 10% melhor que o atual melhor
-                    if tempo_de_vida > Variaveis_globais.melhor_tempo:
-                        Variaveis_globais.melhor_tempo = tempo_de_vida
-                        Variaveis_globais.melhor_individuo = player.camadas
+                        Variaveis_globais.geracao_atual[player.indice] = pesos_individuo
+
+                    else:
+                        Variaveis_globais.geracao_atual[player.indice][0][0] += tempo_de_vida
+
                         
 
     def update(self):
