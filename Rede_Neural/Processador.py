f6a3bfb9eeaced762823fc43260f34475c355e8e
Vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-08-19 12:25:33 -0300

    retirando variaveis utilizadas na classe Player Referanciando classe Player simplificando a camada de saida

--- Rede_Neural/Processador.py
+++ Rede_Neural/Processador.py
@@ -14,17 +14,6 @@ class Processador:
         for arg in args:
             self.camadas.append(arg)
         
-        # variavel para contar a quantidade de loops que o player conseguiu passar
-        self.tick = 0
-
-         # define o ponto de spaw do player
-        self.posicao_x = 750
-        self.posicao_y = 250
-
-        # cria um retandulo de colisão e mostra na tela
-        self.rect_player = pygame.Rect((self.posicao_x - 5, self.posicao_y - 5, 10, 10))
-        draw.rect(tela, (000, 000, 255), (self.posicao_x - 5, self.posicao_y - 5, 10, 10))
-
         # funções de ativação que posem ser usadas
     def funcao_sigmoide(self, entrada):
         saida = 1 / (1 + numpy.exp(-entrada))
@@ -34,15 +23,6 @@ class Processador:
         if entrada < 0:
             entrada = 0
         return entrada
-
-    # pega a quantidade de loops que o player passou e retorna esse valor
-    def funcao_de_perda(self):
-        
-        '''fim = time.time()
-        tempo_normalizado = (fim - self.inicio) / 60'''
-        tempo_em_tick = self.tick
-
-        return tempo_em_tick
 
     # função para retornar as entradas para a rede neural
     def obter_entradas(self):
@@ -55,8 +35,8 @@ class Processador:
             for inimigo in Variaveis_globais.grupo_inimigos:
 
                 informacoes_inimigo = inimigo.informar_posicao()
-                distancia_x = 1 - (abs(informacoes_inimigo[0] - self.rect_player.center[0]) / 110)
-                distancia_y = 1 - (abs(informacoes_inimigo[1] - self.rect_player[1]) / 110)
+                distancia_x = 1 - (abs(informacoes_inimigo[0] - Variaveis_globais.grupo_players[Variaveis_globais.grupo_processadores.index(self)].rect_player.center[0]) / 110)
+                distancia_y = 1 - (abs(informacoes_inimigo[1] - Variaveis_globais.grupo_players[Variaveis_globais.grupo_processadores.index(self)].rect_player.center[1]) / 110)
             
                 distancia = (distancia_x ** 2 + distancia_y ** 2) ** 0.5
 
@@ -85,9 +65,6 @@ class Processador:
     # atualiza o estado do player
     def update(self):
        
-        # conta os loops
-        self.tick += 1
-
         # obtem os resultados dos 8 inimigos mais próximos
         resultados = self.obter_entradas()
 
@@ -95,9 +72,9 @@ class Processador:
         juncao = []
 
         # adiciona como entrada a distancia para cada canto da tela
-        juncao.append((self.rect_player.center[0] / 750) -1)
+        juncao.append((Variaveis_globais.grupo_players[Variaveis_globais.grupo_processadores.index(self)].rect_player.center[0] / 750) -1)
 
-        juncao.append((self.rect_player.center[1] / 250) -1)
+        juncao.append((Variaveis_globais.grupo_players[Variaveis_globais.grupo_processadores.index(self)].rect_player.center[1] / 250) -1)
         for coordenada in resultados:
             for valor in coordenada:
                 juncao.append(valor)
@@ -124,20 +101,5 @@ class Processador:
             
             processamentos_da_rede.append(numpy.array(processamento_da_camada))
         
-        comandos = processamentos_da_rede[-1]
+        self.comandos = processamentos_da_rede[-1]
             
-
-        # as saidas definem a direção que o player vai tomar
-        if comandos[0] > 0:
-            self.posicao_x += velocidade_ia
-        if comandos[1] > 0:
-            self.posicao_x -= velocidade_ia
-
-        if comandos[2] > 0:
-            self.posicao_y += velocidade_ia
-        if comandos[3] > 0:
-            self.posicao_y -= velocidade_ia
-        
-        # cria um retandulo de colisão e mostra na tela
-        self.rect_player = pygame.Rect((self.posicao_x - 5, self.posicao_y - 5, 10, 10))
-        draw.rect(tela, (000, 000, 255), (self.posicao_x - 5, self.posicao_y - 5, 10, 10))  
